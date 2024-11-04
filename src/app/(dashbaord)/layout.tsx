17347e6b5f96dff366ab39e25eb384d155ad3c45
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-04 23:48:55 +0530

    navbar complete

--- src/app/(dashbaord)/layout.tsx
+++ src/app/(dashbaord)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,7 +20,10 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]   '></div>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll  '>
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,42 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div className=''>Navbar</div>;
+  return (
+    <div className='flex  items-center justify-between p-4 '>
+      {/* search bar  */}
+      <div className='hidden md:flex items-center gap-2 text-xs ring-[1.5px] ring-gray-300 px-2 rounded-full'>
+        <Image src='/search.png' width={14} height={14} alt='' />
+        <input
+          type='text'
+          placeholder='search ........'
+          className='w-[200px] p-2 bg-transparent outline-none'
+        />
+      </div>
+      {/* icons & user */}
+      <div className='w-full flex items-center justify-end gap-6 '>
+        <div className='flex justify-center items-center bg-white rounded-full w-7 h-7'>
+          <Image src='/message.png' width={20} height={20} alt='' />
+        </div>
+        <div className='flex justify-center items-center bg-white rounded-full w-7 h-7 relative'>
+          <Image src='/announcement.png' width={20} height={20} alt='' />
+          <div className='absolute -top-3 -right-3 w-5 h-5 rounded-full flex justify-center items-center bg-purple-500 text-white'>
+            3
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <span className='text-xs leading-3 font-medium'>John Doe</span>
+          <span className='text-gray-500 text-[10px] text-right'>Admin</span>
+        </div>
+        <Image
+          src='/avatar.png'
+          alt=''
+          width={36}
+          height={36}
+          className='rounded-full'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
