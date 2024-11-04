b9f8c8716fb1f31131c06aa427b6083970cf227c
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-04 22:23:10 +0530

    dashboard layout hydration problem solved

--- src/app/(dashbaord)/layout.tsx
+++ src/app/(dashbaord)/layout.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className='h-screen flex'>
-        <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200'></div>
-        <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]   bg-blue-200'></div>
-      </body>
-    </html>
+    <div className='h-screen flex'>
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 '>
+        <Link href='/'>
+          {" "}
+          <Image src='/logo.png' alt='logo' height={32} width={32} />
+        </Link>
+      </div>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]   bg-blue-200'></div>
+    </div>
   );
 }
