d94c5f7a0ce8b45932a47cde53dc148f9d6ea1b4
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-04 23:03:09 +0530

    menu ui complete

--- src/app/(dashbaord)/layout.tsx
+++ src/app/(dashbaord)/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from "@/components/Menu";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,14 +8,18 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className='h-screen flex'>
-      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 '>
-        <Link href='/'>
+    <div className='h-screen flex '>
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 border '>
+        <Link
+          href='/'
+          className='flex items-center  justify-center lg:justify-start gap-2 '>
           {" "}
           <Image src='/logo.png' alt='logo' height={32} width={32} />
+          <span className='hidden lg:block'>SchoolLama</span>
         </Link>
+        <Menu />
       </div>
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]   bg-blue-200'></div>
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]   '></div>
     </div>
   );
 }
