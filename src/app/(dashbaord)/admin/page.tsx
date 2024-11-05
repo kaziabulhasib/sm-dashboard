014f787ce762695fe9d19f6502ce632393ab3c8a
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-05 12:24:27 +0530

    admin dashboard outline

--- src/app/(dashbaord)/admin/page.tsx
+++ src/app/(dashbaord)/admin/page.tsx
@@ -1,7 +1,10 @@
 const AdminPage = () => {
   return (
-    <div className=''>
-      <h1>AdminPage</h1>
+    <div className='flex flex-row md:flex-row p-4 gap-4'>
+      {/* left  */}
+      <div className='w-full lg:w-2/3 bg-red-300'>l</div>
+      {/* right  */}
+      <div className='w-full lg:w-1/3 bg-blue-300'>r</div>
     </div>
   );
 };
