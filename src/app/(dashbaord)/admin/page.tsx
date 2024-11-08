5c1b6e2981807498c61597d8fdc1b3597457b806
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-08 22:31:20 +0530

    finance chart

--- src/app/(dashbaord)/admin/page.tsx
+++ src/app/(dashbaord)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 
 import UserCard from "@/components/UserCard";
 
@@ -27,7 +28,9 @@ const AdminPage = () => {
           </div>
         </div>
         {/* BOTTOM CHART  */}
-        <div className='w-full lg:w-2/3 h-[450px]'></div>
+        <div className='w-full  h-[500px]'>
+          <FinanceChart />
+        </div>
       </div>
       {/* right  */}
       <div className='w-full lg:w-1/3 bg-blue-300'>r</div>
