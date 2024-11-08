635e422776fed3d80dae36bbd32a4dcbb738a4ac
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-08 23:12:13 +0530

    react calender customization

--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
 import Calendar from "react-calendar";
 
@@ -10,7 +10,7 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 const EventCalender = () => {
   const [value, onChange] = useState<Value>(new Date());
   return (
-    <div className=''>
+    <div className='bg-white p-4 rounded-md'>
       <Calendar onChange={onChange} value={value} />
     </div>
   );
