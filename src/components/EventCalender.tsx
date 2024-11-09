b53fd79972179ec7ae77f5f9959e34f7b2dc4a38
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-09 10:20:05 +0530

    events section

--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -2,16 +2,56 @@
 import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
 import Calendar from "react-calendar";
+import Image from "next/image";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+// TEMPORARY
+const events = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
 
 const EventCalender = () => {
   const [value, onChange] = useState<Value>(new Date());
   return (
     <div className='bg-white p-4 rounded-md'>
       <Calendar onChange={onChange} value={value} />
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-semibold my-4'>Events</h1>
+        <Image src='/moreDark.png' alt='' width={20} height={20} />
+      </div>
+      <div className='flex flex-col gap-4 '>
+        {events.map((event) => (
+          <div
+            className='p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple'
+            key={event.id}>
+            <div className='flex justify-between my-2'>
+              <h1 className=' font-semibold'>{event.title}</h1>
+              <p className='text-xs text-gray-300'>{event.time}</p>
+            </div>
+            <p className='text-sm text-gray-500'>{event.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
