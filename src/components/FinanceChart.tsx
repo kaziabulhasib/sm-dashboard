879757a3edbb9f5f1a4cf532db6fcbd051087385
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-09 10:33:30 +0530

    admin dashboard complete

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -70,9 +70,9 @@ const data = [
 ];
 const FinanceChart = () => {
   return (
-    <div className='bg-white rounded-lg p-4 h-full w-full'>
+    <div className='bg-white rounded-lg p-2 md:p-4 h-full w-full'>
       <div className='flex justify-between items-center'>
-        <h1 className='text-lg font-semibold'>Attendance</h1>
+        <h1 className='text-lg font-semibold'>Finance</h1>
         <Image src='/moreDark.png' width={20} height={20} alt='' />
       </div>
       <ResponsiveContainer width='100%' height='90%'>
