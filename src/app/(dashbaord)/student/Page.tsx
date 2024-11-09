7d9433fd0ba2df8ecbae6f6abcd9208181b396f3
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-09 10:53:27 +0530

    student right side

--- src/app/(dashbaord)/student/Page.tsx
+++ src/app/(dashbaord)/student/Page.tsx
@@ -1,5 +1,19 @@
+import Announcements from "@/components/Announcements";
+import EventCalender from "@/components/EventCalender";
+
 const StudentPage = () => {
-  return <div className=''>StudentPage</div>;
+  return (
+    <div className='flex flex-col xl:flex-row p-4 gap-4'>
+      {/* LEFT  */}
+      <div className='w-full xl:w-2/3'></div>
+      {/* RIGHT  */}
+
+      <div className='w-full xl:w-1/3 flex flex-col gap-4'>
+        <EventCalender />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
