a79ecafd9fce181cc416fd7c063b542f9352262e
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-10 21:59:37 +0530

    custom heading

---
+++ src/components/CustomHead.tsx
@@ -0,0 +1,10 @@
+const CustomHead = () => {
+  return (
+    <div className=''>
+      {/* To do : a re usable head components to use throughout the pages */}
+      CustomHead
+    </div>
+  );
+};
+
+export default CustomHead;
