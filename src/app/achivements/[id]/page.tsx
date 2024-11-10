d15a835d7087cd615fc1131e598f707175a6e9f7
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-10 22:06:21 +0530

    type set fro params

--- src/app/achivements/[id]/page.tsx
+++ src/app/achivements/[id]/page.tsx
@@ -1,4 +1,8 @@
-const AchivementPage = ({ params }) => {
+type MyParams = {
+  id: string;
+};
+
+const AchivementPage = ({ params }: { params: MyParams }) => {
   return (
     <div className=''>
       <h1>achivement no : {params.id}</h1>
