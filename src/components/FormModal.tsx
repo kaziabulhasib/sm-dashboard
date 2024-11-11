a31bd06bdd82922be313507c2b96f6f272062c30
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-11 22:44:32 +0530

    modal open & close

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 const FormModal = ({
   table,
@@ -8,7 +9,19 @@ const FormModal = ({
   data,
   id,
 }: {
-  table: "teacher" | "student";
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
   type: "create" | "update" | "delete";
   data?: any;
   id?: number;
@@ -20,12 +33,26 @@ const FormModal = ({
       : type === "update"
       ? "bg-lamaSky"
       : "bg-lamaPurple";
+
+  const [open, setOpen] = useState(false);
   return (
     <>
       <button
+        onClick={() => setOpen(true)}
         className={`${size} flex items-center justify-center rounded-full ${bgColor} `}>
         <Image src={`/${type}.png`} alt='' width={16} height={16} />
       </button>
+      {open && (
+        <div className='w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center'>
+          <div className='bg-white rounded-md p-4 relative w-[90%] md:w-[70%] xl:w-[50%]'>
+            <div
+              className='absolute top-4 right-4 cursor-pointer '
+              onClick={() => setOpen(false)}>
+              <Image src='/close.png' alt='' width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
