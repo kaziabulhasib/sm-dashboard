f5fbc39781233e450c13991eb619a42115895ed5
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-11 19:51:05 +0530

    single teacher page start up

---
+++ src/app/(dashbaord)/list/teachers/[id]/page.tsx
@@ -0,0 +1,12 @@
+const SingleTeacherPage = () => {
+  return (
+    <div className='flex-1 p-4 flex flex-col xl:flex-row  gap-4'>
+      {/* left  */}
+      <div className='w-full xl:w-2/3'>l</div>
+      {/* right  */}
+      <div className='w-full xl:w-1/3'>r</div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
