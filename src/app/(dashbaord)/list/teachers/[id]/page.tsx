8520d1f76584f656061580088c40c46b469a2886
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-11 20:26:45 +0530

    shortcut & annoucement

--- src/app/(dashbaord)/list/teachers/[id]/page.tsx
+++ src/app/(dashbaord)/list/teachers/[id]/page.tsx
@@ -1,4 +1,7 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
 import Image from "next/image";
+import Link from "next/link";
 
 const SingleTeacherPage = () => {
   return (
@@ -103,11 +106,37 @@ const SingleTeacherPage = () => {
             </div>
           </div>
         </div>
-        {/* Bottom  */}
-        <div className=''>Schedule</div>
+        {/* BOTTOM */}
+        <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
       </div>
       {/* right  */}
-      <div className='w-full xl:w-1/3'>r</div>
+      <div className='w-full xl:w-1/3 flex flex-col gap-4'>
+        <div className='bg-white p-4 rounded-md'>
+          <h1 className='text-xl font-semibold'>Shortcuts</h1>
+          <div className='mt-4 flex gap-4 flex-wrap text-xs text-gray-500'>
+            <Link className='p-3 rounded-md bg-lamaSkyLight' href='/'>
+              Teacher&apos;s Classes
+            </Link>
+            <Link className='p-3 rounded-md bg-lamaPurpleLight' href='/'>
+              Teacher&apos;s Students
+            </Link>
+            <Link className='p-3 rounded-md bg-lamaYellowLight' href='/'>
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className='p-3 rounded-md bg-pink-50' href='/'>
+              Teacher&apos;s Exams
+            </Link>
+            <Link className='p-3 rounded-md bg-lamaSkyLight' href='/'>
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        {/* <Performance /> */}
+        <Announcements />
+      </div>
     </div>
   );
 };
