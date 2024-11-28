916de42b490a40798d6ef72e6ed636aebfb4f286
kaziabulhasib <hasibkazi420@gmail.com>
2024-11-28 11:22:00 +0530

    admin page route in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className=''>HomePage</div>;
+  return (
+    <div className=''>
+      <h1 className='text-2xl text-center py-6'>
+        School Management System HomePage
+      </h1>
+      <div className='flex justify-center'>
+        <Link className='py-8 text-center hover:underline ' href='/admin'>
+          Go To admin Page
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
